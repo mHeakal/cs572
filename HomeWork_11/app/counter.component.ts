f5b48409e50b0964f5345deeb3c860f9b3f5517b
mHeakal <46614643+mHeakal@users.noreply.github.com>
2019-01-28 14:50:10 -0600

    Angular counter component

---
+++ HomeWork_11/app/counter.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent implements OnInit {
+  @Input() counterValue: number;
+  constructor() { 
+    this.counterValue = 0;
+  }
+  ngOnInit() {
+    // console.log('Counter is workings')
+  }
+  inc(){
+    this.counterValue++;
+    return false;
+  }
+  dec(){
+    if(this.counterValue > 0)
+    this.counterValue--;
+    return false;
+  }
+}
